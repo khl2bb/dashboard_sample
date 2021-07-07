21ed70f163c823d8316ce4f6e28bb21a69c3429e
khl2bb <43461931+khl2bb@users.noreply.github.com>
2021-07-08 02:42:12 +0900

    커밋 초기화
    This reverts commit 2241cbb0875e7da38fc164602163e3c264482bf2.

--- js/appSelect.js
+++
@@ -1,8 +0,0 @@
-const $appSelectBox = document.getElementById("appSelect");
-const $targetBox = document.getElementById("sideBoxCon2");
-
-function appSelectChange(e) {
-  $targetBox.innerText = e.target.value;
-}
-
-$appSelectBox.addEventListener("change", appSelectChange);
